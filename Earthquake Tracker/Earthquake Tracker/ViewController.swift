fb2607b11c9fa9625e8e1597d057ed4377d9a8c7
Halil Ibrahim Andic <hiandic@gmail.com>
2022-12-30 23:40:50 +0300

    initial settings applied

---
+++ Earthquake Tracker/Earthquake Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Earthquake Tracker
+//
+//  Created by Halil Ibrahim Andic on 30.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
